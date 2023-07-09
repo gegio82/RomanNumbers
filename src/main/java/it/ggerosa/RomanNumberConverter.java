e13097a653dd4c58dc6dcd343d1d5694d9d785d4
gegio82 <giorgio.gerosa@gmail.com>
2023-07-09 08:25:51 +0100

    fixing test for special value 4

--- src/main/java/it/ggerosa/RomanNumberConverter.java
+++ src/main/java/it/ggerosa/RomanNumberConverter.java
@@ -14,12 +14,13 @@ public class RomanNumberConverter {
     }
 
     enum RomanSymbol {
-        X('X', 10),
-        V('V', 5),
-        I('I', 1);
-        char symbol;
+        X("X", 10),
+        V("V", 5),
+        IV("IV", 4),
+        I("I", 1);
+        String symbol;
         int value;
-        RomanSymbol(char symbol, int value) {
+        RomanSymbol(String symbol, int value) {
             this.symbol = symbol;
             this.value = value;
         }
