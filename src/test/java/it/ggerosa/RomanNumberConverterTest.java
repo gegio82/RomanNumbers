80df7d566d04dad0bc77d1f1e66a21b2418e157b
gegio82 <giorgio.gerosa@gmail.com>
2023-07-09 07:47:01 +0100

    first test

--- src/test/java/it/ggerosa/RomanNumberConverterTest.java
+++ src/test/java/it/ggerosa/RomanNumberConverterTest.java
@@ -1,5 +1,15 @@
 package it.ggerosa;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class RomanNumberConverterTest {
 
+    private final RomanNumberConverter converter = new RomanNumberConverter();
+
+    @Test
+    void canConvertNumber1() {
+        assertThat(converter.toRoman(1)).isEqualTo("I");
+    }
 }
