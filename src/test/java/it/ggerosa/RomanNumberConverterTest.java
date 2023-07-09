5b0ebd0a7dbb00efe7f8aff440f79c7c195712e7
gegio82 <giorgio.gerosa@gmail.com>
2023-07-09 08:24:29 +0100

    test for special value 4

--- src/test/java/it/ggerosa/RomanNumberConverterTest.java
+++ src/test/java/it/ggerosa/RomanNumberConverterTest.java
@@ -14,6 +14,7 @@ class RomanNumberConverterTest {
             "1, I",
             "2, II",
             "3, III",
+            "4, IV",
             "5, V",
             "6, VI",
             "7, VII",
