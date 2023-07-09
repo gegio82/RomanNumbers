8c171f52fb7bb913cc7591f522c546a6b6090f68
gegio82 <giorgio.gerosa@gmail.com>
2023-07-09 08:20:01 +0100

    refactor: add I to RomanSymbol enum

--- src/main/java/it/ggerosa/RomanNumberConverter.java
+++ src/main/java/it/ggerosa/RomanNumberConverter.java
@@ -5,19 +5,18 @@ public class RomanNumberConverter {
     public String toRoman(int decimal) {
         StringBuilder builder = new StringBuilder();
         for (RomanSymbol romanSymbol: RomanSymbol.values()) {
-            if (decimal >= romanSymbol.value) {
+            while (decimal >= romanSymbol.value) {
                 builder.append(romanSymbol.symbol);
                 decimal -= romanSymbol.value;
             }
-        }
-        for (int i = 0; i < decimal; i++) {
-            builder.append("I");
         }
         return builder.toString();
     }
 
     enum RomanSymbol {
-        X('X', 10), V('V', 5);
+        X('X', 10),
+        V('V', 5),
+        I('I', 1);
         char symbol;
         int value;
         RomanSymbol(char symbol, int value) {
